e1b6e41cf22925930f2b6334be59bb512f55537b
BIT <BIT@DESKTOP-D7VRVOQ>
2019-11-25 19:17:23 +0900

    20191125

---
+++ src/chapter04/ObjectTest.java
@@ -0,0 +1,15 @@
+package chapter04;
+
+public class ObjectTest {
+	
+	public static void main(String[] args) {
+		
+		Point p = new Point(10,20);
+		
+		System.out.println(p.getClass()); // reflection // 객체가 어떻게 생겨났나, 클래스 정보
+		System.out.println(p.hashCode()); // hashCode값, Reference Value
+		System.out.println(p);
+		System.out.println(p.toString());
+	}
+
+}
